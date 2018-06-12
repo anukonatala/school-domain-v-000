321d5705ab03ea9bacbc678adec996d4b3f3b711
Anusha Konatala <anu.konatala@gmail.com>
2018-06-12 22:50:17 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
 # code here!
+
+class School
+  hash = {}
+  
+  def initialize(hash)
+  
+  end
+  def add_student(name, grade)
+    if  hash.key?(grade)
+      hash[grade] << name
+    else
+      hash[grade] = []
+    end
+  end
+  
+  def roster
+    hash
+  end
+  
+  def sort
+    hash.sort.to_h
+  end
+
+end
